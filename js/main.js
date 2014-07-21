d9ffab122f4dbeb5d430a26626ab39fac67decf7
Andraž Hostnik <andraz@glit.si>
2014-07-21 15:20:25 +0200

    styling #2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,42 @@
-
-
-/*
-function receive(saveData) {
-    if (saveData == null) {
-        alert("DATA IS UNDEFINED!");  // displays every time
-    }
-    alert("Success is " + saveData);  // 'Success is undefined'
-}
-
 var jsonURL = "http://test.enyo.av-studio.net/products.ashx";
+
 $.ajax({
-    data: {  },
-    cache: false,
-    dataType: "json", // tried json
     type: "GET",
+    dataType: "jsonp", // tried json
+    jsonpCallback: 'jsonCallback',
     crossDomain: true,
     url: jsonURL,
     async: false,
+    success: function(json) {
+        $.each(json.products, function( index, item ) {
+            var itemHtml1 = "<div class='col-xs-6 col-md-4 product-box'>" +
+                "<a href='#'><img src=\""+ item.image +"\"></a>" +
+                "<div class='product-title'><span>"+ item.name +"</span></div>";
+
+            var price = "";
+            if (!item.action_price) {
+                price = "<div class='product-price-box'>" + item.regular_price + " €</div>";
+            } else {
+                price = "<div class='product-price-box'>" + item.action_price + " €" +
+                    "<span class='old-price'>" + item.regular_price + " €" + "</span></div>";
+            }
+
+            var itemHtml2 =
+                "<div class='row'><div class='product-quantity-box input-group col-sm-6'>" +
+                "<span class='input-group-btn'><button class='btn btn-default' type='button'>-</button></span>" +
+                "<input type='text' class='form-control'>" +
+                "<span class='input-group-btn'>" +
+                "<button class='btn btn-default' type='button'>+</button>"+
+                "</span></div>" +
+                "<div class='col-sm-6'><a class='btn btn-cart btn-block' href='#'>KUPIM</a></div>"
+                "</div>";
+            $(".category-view .product-list").append(itemHtml1 + price + itemHtml2);
+        });
+    },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-        console.log(textStatus, errorThrown);
+        console.log(textStatus);
     }
 });
-*/
-/*
-$.getJSON( jsonURL, function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-        items.push( "<li id='" + key + "'>" + val + "</li>" );
-    });
 
-    $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-    }).appendTo( "body" );
-});
-*/
+
+$('#filters').collapse();
